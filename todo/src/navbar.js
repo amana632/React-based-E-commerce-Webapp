1018636ffdab722056a1d3fc87a836a57614dd3f
rashi13 <rashiagrawal1308@gmail.com>
2018-06-14 03:59:40 +0530

    rashi

--- todo/src/navbar.js
+++ todo/src/navbar.js
@@ -1,41 +1,121 @@
 import React, { Component } from "react";
 import header1 from "./images/header-1.jpg";
 import header2 from "./images/header-2.jpg";
+import header3 from "./images/header-3.jpg";
 
 export class Navbar extends Component {
   render() {
     return [
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Navbar
+          Tech__Giant
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Comp Accessories
               </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item" href="#">
+                  Barcode_scanner
+                </a>
+                <a className="dropdown-item" href="#">
+                  CPU Holder
+                </a>
+                <a className="dropdown-item" href="#">
+                  Card Reader
+                </a>
+                <a className="dropdown-item" href="#">
+                  Cleaning Kit
+                </a>
+
+                <a className="dropdown-item" href="#">
+                  Combos
+                </a>
+                <a className="dropdown-item" href="#">
+                  HD Enclosures
+                </a>
+                <a className="dropdown-item" href="#">
+                  Laptop Accessories Combos
+                </a>
+                <a className="dropdown-item" href="#">
+                  Laptop Skin Decals
+                </a>
+                <a className="dropdown-item" href="#">
+                  Monitor TV Cover
+                </a>
+                <a className="dropdown-item" href="#">
+                  Monitor Arms
+                </a>
+                <a className="dropdown-item" href="#">
+                  Pen Tablets
+                </a>
+                <a className="dropdown-item" href="#">
+                  Printer Covers
+                </a>
+                <a className="dropdown-item" href="#">
+                  USB Gadgets
+                </a>
+              </div>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <a className="nav-link" href="#">
                 Features
               </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
+            </li> */}
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Peripherals
               </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item" href="#">
+                  Portable projector
+                </a>
+                <a className="dropdown-item" href="#">
+                  Portable Scanner
+                </a>
+
+                <a className="dropdown-item" href="#">
+                  Printer/Scanner Pocket Printer
+                </a>
+                <a className="dropdown-item" href="#">
+                  Printer/Scanner Printer Filaments
+                </a>
+                <a className="dropdown-item" href="#">
+                  Printer/Scanner Printer MultiF
+                </a>
+                <a className="dropdown-item" href="#">
+                  Printer/Scanner Printer Single
+                </a>
+                <a className="dropdown-item" href="#">
+                  Printer/Scanner Receipt printer
+                </a>
+                <a className="dropdown-item" href="#">
+                  Scanners
+                </a>
+              </div>
             </li>
             <li className="nav-item dropdown">
               <a
@@ -46,20 +126,245 @@ export class Navbar extends Component {
                 aria-haspopup="true"
                 aria-expanded="false"
               >
-                Dropdown link
+                PC&Laptop
               </a>
               <div
                 className="dropdown-menu"
                 aria-labelledby="navbarDropdownMenuLink"
               >
                 <a className="dropdown-item" href="#">
-                  Action
+                  All in One PCs
                 </a>
                 <a className="dropdown-item" href="#">
-                  Another action
+                  Tower PCs
                 </a>
                 <a className="dropdown-item" href="#">
-                  Something else here
+                  Laptops
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Laptop Accessories
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item" href="#">
+                  Batteries
+                </a>
+                <a className="dropdown-item" href="#">
+                  Cleaning Kits
+                </a>
+                <a className="dropdown-item" href="#">
+                  Cooling Pads/stands
+                </a>
+                <a className="dropdown-item" href="#">
+                  Headphones
+                </a>
+                <a className="dropdown-item" href="#">
+                  OTG Adapters
+                </a>
+                <a className="dropdown-item" href="#">
+                  PowerBanks
+                </a>
+                <a className="dropdown-item" href="#">
+                  World Wide Adapters
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Network&Cables
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item" href="#">
+                  Access Points
+                </a>
+                <a className="dropdown-item" href="#">
+                  Antenna Amplifiers
+                </a>
+                <a className="dropdown-item" href="#">
+                  Cables
+                </a>
+                <a className="dropdown-item" href="#">
+                  Data Cards
+                </a>
+                <a className="dropdown-item" href="#">
+                  LAN Adapters
+                </a>
+                <a className="dropdown-item" href="#">
+                  Network Interface Cards
+                </a>
+                <a className="dropdown-item" href="#">
+                  Network Servers
+                </a>
+                <a className="dropdown-item" href="#">
+                  Routers Antenna Boosters
+                </a>
+                <a className="dropdown-item" href="#">
+                  Routers
+                </a>
+                <a className="dropdown-item" href="#">
+                  Switches
+                </a>
+                <a className="dropdown-item" href="#">
+                  Wireless USB Adapters
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Software
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item" href="#">
+                  Business Productivity
+                </a>
+                <a className="dropdown-item" href="#">
+                  Educational Media Physical
+                </a>
+                <a className="dropdown-item" href="#">
+                  Multimedia
+                </a>
+                <a className="dropdown-item" href="#">
+                  Multimedia Physical
+                </a>
+                <a className="dropdown-item" href="#">
+                  Office Tools Physical
+                </a>
+                <a className="dropdown-item" href="#">
+                  Office Tools Physical Key Cards
+                </a>
+                <a className="dropdown-item" href="#">
+                  Office Tools Physical Perpetual License
+                </a>
+                <a className="dropdown-item" href="#">
+                  Operating Systems
+                </a>
+                <a className="dropdown-item" href="#">
+                  Operating Systems Digital
+                </a>
+                <a className="dropdown-item" href="#">
+                  Operating Systems Physical
+                </a>
+                <a className="dropdown-item" href="#">
+                  Security Software Antivirus
+                </a>
+                <a className="dropdown-item" href="#">
+                  Security Software Digital
+                </a>
+                <a className="dropdown-item" href="#">
+                  Security Software Internet Security
+                </a>
+                <a className="dropdown-item" href="#">
+                  Security Software Total Security
+                </a>
+                <a className="dropdown-item" href="#">
+                  Utilities Digital
+                </a>
+                <a className="dropdown-item" href="#">
+                  Utilities Physical
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Storage
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item" href="#">
+                  External HDD
+                </a>
+                <a className="dropdown-item" href="#">
+                  Internal HDD
+                </a>
+                <a className="dropdown-item" href="#">
+                  Pendrives
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Supplies
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item" href="#">
+                  Inks
+                </a>
+                <a className="dropdown-item" href="#">
+                  Toners
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Tab Accessories
+              </a>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a className="dropdown-item" href="#">
+                  Cables
+                </a>
+                <a className="dropdown-item" href="#">
+                  Memory Cards
                 </a>
               </div>
             </li>
